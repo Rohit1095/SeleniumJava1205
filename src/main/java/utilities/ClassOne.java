144078b4078da9abf43dface15c173749b69fb8f
Rohit <rathorerohit95@gmail.com>
2023-12-05 21:53:53 +0530

    first commit in the branch

---
+++ src/main/java/utilities/ClassOne.java
@@ -0,0 +1,8 @@
+package utilities;
+
+public class ClassOne {
+	public static void main(String[] args) {
+		System.out.println("class one");
+	}
+
+}
---
+++ src/test/java/samplePkg/ClassTwo.java
@@ -0,0 +1,8 @@
+package samplePkg;
+
+public class ClassTwo {
+	public static void main(String[] args) {
+		System.out.println("class two");
+	}
+
+}
